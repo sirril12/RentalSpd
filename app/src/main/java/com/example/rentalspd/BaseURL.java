d7948b075be8b75b317e36be77c37f7f9d528f3a
sirril12 <sirrilwafa2018rplrus2@gmail.com>
2020-10-24 14:46:13 +0700

    text

--- app/src/main/java/com/example/rentalspd/BaseURL.java
+++ app/src/main/java/com/example/rentalspd/BaseURL.java
@@ -2,5 +2,5 @@ package com.example.rentalspd;
 
 public class BaseURL {
 
-    public static final String url="http://192.168.43.254/Api/";
+    public static final String url="http://192.168.43.152/Api/";
 }
